40956aad5725067793aa6b5efacca6c3cdae94d7
Richard Kille <richard.kille@bbc.co.uk>
2026-01-08 12:23:37 +0000

    Agent update to remote changes

---
+++ aoc2025/day1/day1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"advent-of-code/utils"
+	"fmt"
+)
+
+func main() {
+	input := utils.GetInput("aoc2025/day1/day1_ex.txt")
+  
+	for _, line := range input {
+    	// code here
+		fmt.Println(line)
+	}
+}
