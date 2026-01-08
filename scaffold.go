cbe88d0c8b4efe66297ddf0a9b51ca732f67c7ab
Richard Kille <richard.kille@bbc.co.uk>
2026-01-08 12:21:28 +0000

    Handle Agent updates to scaffold.go

--- scaffold.go
+++ scaffold.go
@@ -17,8 +17,8 @@ import (
 	"fmt"
 )
 
-func day%[1]d() {
-	input := utils.GetInput("aoc%[2]d/day%[1]d_ex.txt")
+func main() {
+	input := utils.GetInput("aoc%[2]d/day%[1]d/day%[1]d_ex.txt")
   
 	for _, line := range input {
     	// code here
@@ -27,24 +27,24 @@ func day%[1]d() {
 }
 `, day, year)
 
-	err := os.WriteFile(fmt.Sprintf("aoc%d/day%d.go", year, day), []byte(goTemplate), 0644)
+	err := os.WriteFile(fmt.Sprintf("aoc%d/day%d/day%d.go", year, day, day), []byte(goTemplate), 0644)
 	if err != nil {
 		log.Fatalln("failed to create go file", err)
 	}
 
 	exTxtTemplate := fmt.Sprint(`// example text here`)
-	err = os.WriteFile(fmt.Sprintf("aoc%d/day%d_ex.txt", year, day), []byte(exTxtTemplate), 0644)
+	err = os.WriteFile(fmt.Sprintf("aoc%d/day%d/day%d_ex.txt", year, day, day), []byte(exTxtTemplate), 0644)
 	if err != nil {
 		log.Fatalln("failed to create example text file", err)
 	}
 
 	txtTemplate := fmt.Sprint(`// input text here`)
-	err = os.WriteFile(fmt.Sprintf("aoc%d/day%d.txt", year, day), []byte(txtTemplate), 0644)
+	err = os.WriteFile(fmt.Sprintf("aoc%d/day%d/day%d.txt", year, day, day), []byte(txtTemplate), 0644)
 	if err != nil {
 		log.Fatalln("failed to create input text file", err)
 	}
 }
 
 func main() {
-	scaffold(2)
+	scaffold(1)
 }
